442c6325b49c29f63dbab0e57814ae199db90ff2
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-03-13 21:10:48 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+//Logical Oprator && ||
+
+//&& Oprator
+let a: number = 10;
+let b: number = 10;
+console.log(a == b && b == a);//True
+console.log(a < b && b > a);//False
+
+
+//|| Oprator
+let c: number = 10;
+let d: number = 5;
+console.log(c > d || d < c);//True
+
+//! Oprtor Not
+let e: number = 10;
+console.log!(e==0);//False
